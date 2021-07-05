a648e3f1025dcaaa950e14a1685a069d20f0f71e
kurtisjin <cloudnine21@gmail.com>
2021-07-04 22:20:35 -0500

    added folders and started landing page. using material.ui

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core/';
+
+import Header from "./component/Header/Header"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: '100vh',
+        backgroundImage: `url(${process.env.PUBLIC_URL + "cooking1.jpg"})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: 'cover',
+    }
+}));
+
+function App() {
+    const classes = useStyles();
+    return <div className={classes.root}>
+        <CssBaseline />
+        <Header />
+    </div>
+}
+
+export default App
