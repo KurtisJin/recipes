bc1dad9bb1962ba90163891fa5dda061051eddc1
kurtisjin <cloudnine21@gmail.com>
2021-07-25 14:20:24 -0500

    redesigned the login page

--- client/src/Pages/Signup/Signup.js
+++ client/src/Pages/Signup/Signup.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles, createStyles} from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -10,27 +13,44 @@ const useStyles = makeStyles((theme) =>
         width: '25ch',
       },
     },
+    main: {
+      backgroundImage: `url(${process.env.PUBLIC_URL + "cooking2.jpg"})`,
+      // backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      height: "45vh",
+      width: "100vh",
+      marginTop: "15%",
+      marginLeft: "25%",
+    },
+
+    title: {
+      fontSize: "35px",
+      marginBottom: "50px"
+    },
+    
   }),
 );
  function Signup() {
   const classes = useStyles();
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        
-        <TextField required id="standard-required" label="Email" defaultValue="" />
-        <TextField
-          id="standard-password-input"
-          required id="standard-required"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        
-        
-      </div>
-    </form>
+    <div className={classes.main}>
+      <h1 className={classes.title}>Please login using your Email and Password</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+          <TextField required id="standard-required" label="Email" defaultValue="" />
+          <TextField
+            id="standard-password-input"
+            required id="standard-required"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+          <input type="submit" value="Submit" className="btn btn-success" />
+          
+          <input type="return" value="Return" className="btn btn-success" />  
+            
+      </form>
+    </div>
   );
 }
 
